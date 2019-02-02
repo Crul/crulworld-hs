e98043c3320b112e7d40c25c05f4d9b041bfe091
Crul <rauljavier.vila@gmail.com>
2019-02-02 09:39:27 +0100

    dummy simulation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE ExtendedDefaultRules #-}
+module Lib (someFunc) where
+
+import Conduit
+import Data.List
+
+beginningOfTime = 1
+endOfTime = 10
+
+data WorldData = WorldData { _time :: Int } deriving (Show)
+time :: WorldData -> Int
+time (WorldData t) = t
+
+runTimeStep :: WorldData -> WorldData
+runTimeStep w = w { _time = succ $ time w }
+
+  -- RIGHT !!!
+runTimeSteps :: WorldData -> [WorldData]
+runTimeSteps w@(WorldData t) | t == endOfTime = [w]
+runTimeSteps w = w : runTimeSteps (runTimeStep w)
+  -- WRONG !!!
+  -- runTimeSteps :: WorldData -> [WorldData] -> [WorldData]
+  -- runTimeSteps w@(WorldData t) xs | t == endOfTime = w:xs
+  -- runTimeSteps w xs = runTimeSteps (runTimeStep w) (w:xs)
+  -- WRONG !!!
+  -- runTimeSteps :: WorldData -> [WorldData] -> [WorldData]
+  -- runTimeSteps w@(WorldData t) xs | t == endOfTime = xs ++ [w]
+  -- runTimeSteps w xs = runTimeSteps (runTimeStep w) (xs ++ [w])
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  runConduit
+    $ yieldMany (runTimeSteps (WorldData beginningOfTime))
+    .| mapM_C print
