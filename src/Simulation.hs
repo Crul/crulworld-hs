21e879ad6f128c53a05e67ba6027d1e1a3c3ff98
Crul <rauljavier.vila@gmail.com>
2019-02-03 11:09:55 +0100

    2D vector

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -4,16 +4,29 @@ beginningOfTime = 1
 endOfTime       = 9
 agentsSpeed     = 1.5  -- TODO make speed work properly (now it applies to x,y not to the real move)
 
+data Vector = Vector {x :: Float, y :: Float}
+instance Show Vector where
+  show v = "(" ++ (show $ x v) ++ "," ++ (show $ y v) ++ ")"
+  
+vecAdd :: Vector -> Vector -> Vector
+vecAdd v1 v2 = Vector ((x v1) + (x v2)) ((y v1) + (y v2))
+
+newtype Position = Position Vector deriving (Show)
+getPosVector :: Position -> Vector
+getPosVector (Position v) = v
+
+newtype Movement = Movement Vector
+getMovVector :: Movement -> Vector
+getMovVector (Movement v) = v
+
 type Time = Int
 
 data World = World { time :: Int, agents :: [Agent] } deriving (Show)
+
 data AgentType = Prey | Hunter | Food deriving (Eq, Show)
-data Agent = Agent {
-  agentType :: AgentType,
-  agentId   :: Int,
-  xAgPos    :: Float,
-  yAgPos    :: Float
-}
+data Agent = Agent { agentType :: AgentType, agentId :: Int, agentPos :: Position }
+instance Show Agent where
+  show a = (show $ agentType a) ++ "[" ++ (show $ agentId a) ++ "]" ++ (show $ getPosVector $ agentPos a)
 
 isFood :: Agent -> Bool
 isFood a = agentType a == Food
@@ -21,43 +34,35 @@ isFood a = agentType a == Food
 isPrey :: Agent -> Bool
 isPrey a = agentType a == Prey
 
-instance Show Agent where
-  show a = (show $ agentType a)
-           ++ "[" ++ (show $ agentId a) ++ "]"
-           ++ "(" ++ (show $ xAgPos a) ++ "," ++ (show $ yAgPos a) ++ ")"
-
-data Move = Move { xMove :: Float, yMove :: Float }
-
 updateWorld :: World -> World
 updateWorld w = w { time = t, agents = ags }
   where
     t         = succ $ time w
     agsAndMvs = map (updateAgent w) (agents w)
-    ags       = map applyMoveAction agsAndMvs
+    ags       = map applyMovement agsAndMvs
 
 senseAgents :: Agent -> World -> [Agent]
 senseAgents _ w = agents w  -- TODO filter agents based on agent position and vision-range parameter
 
-updateAgent :: World -> Agent -> (Agent, Move)
-updateAgent w a@(Agent Prey   _ _ _) = updateTowardsType isFood w a
-updateAgent w a@(Agent Hunter _ _ _) = updateTowardsType isPrey w a
-updateAgent w a@(Agent Food   _ _ _) = (a, Move 0 0)  -- TODO Food shouldn't do actions
+updateAgent :: World -> Agent -> (Agent, Movement)
+updateAgent w a@(Agent Prey   _ _) = updateTowardsType isFood w a
+updateAgent w a@(Agent Hunter _ _) = updateTowardsType isPrey w a
+updateAgent w a@(Agent Food   _ _) = (a, Movement (Vector 0 0))  -- TODO Food shouldn't do actions
 
-updateTowardsType :: (Agent -> Bool) -> World -> Agent -> (Agent, Move)
+updateTowardsType :: (Agent -> Bool) -> World -> Agent -> (Agent, Movement)
 updateTowardsType tf w a = ( nxtA, mv )
   where
     ags  = senseAgents a w
     trgt = head $ filter tf ags
-    mvTw = moveTowardsAg a trgt
-    mv   = Move (fst mvTw) (snd mvTw)
+    mv   = moveTowardsAg a trgt
     nxtA = a
 
-moveTowardsAg :: Agent -> Agent -> (Float, Float)
-moveTowardsAg ag trgt = ( xMv, yMv )
+moveTowardsAg :: Agent -> Agent -> Movement
+moveTowardsAg ag trgt = Movement $ Vector xMv yMv
   where
     moveAgTowardsAg = moveTowardsAgComp ag trgt
-    xMv = moveAgTowardsAg xAgPos
-    yMv = moveAgTowardsAg yAgPos
+    xMv = moveAgTowardsAg (x . getPosVector . agentPos)
+    yMv = moveAgTowardsAg (y . getPosVector . agentPos)
 
 moveTowardsAgComp :: Agent -> Agent -> (Agent -> Float) -> Float
 moveTowardsAgComp ag trgt agPFn =
@@ -66,16 +71,17 @@ moveTowardsAgComp ag trgt agPFn =
     else agentsSpeed * (dst / abs dst)
   where dst = (agPFn trgt) - (agPFn ag)
 
-applyMoveAction :: (Agent, Move) -> Agent
-applyMoveAction (a, m) = a {
-    xAgPos = (xAgPos a) + (xMove m),
-    yAgPos = (yAgPos a) + (yMove m)
-  }
+applyMovement :: (Agent, Movement) -> Agent
+applyMovement (ag, mv) = ag { agentPos = newPos }
+  where
+    agPos   = getPosVector $ agentPos ag
+    trgtPos = getMovVector mv
+    newPos  = Position $ agPos `vecAdd` trgtPos
 
 initialAgents = [
-    (Agent Food   1 8 8),
-    (Agent Prey   1 0 0),
-    (Agent Hunter 1 4 0)
+    (Agent Food   1 (Position (Vector 8 8))),
+    (Agent Prey   2 (Position (Vector 0 0))),
+    (Agent Hunter 3 (Position (Vector 4 0)))
   ]
 
 initialWorld :: World
@@ -88,4 +94,4 @@ updateTimeLineUpTo :: Time -> [World]
 updateTimeLineUpTo i = takeWhile (\x -> time x <= i) timeline
 
 runSimulation :: IO ()
-runSimulation = mapM_ print (updateTimeLineUpTo 10)
+runSimulation = mapM_ print (updateTimeLineUpTo endOfTime)
