33cd964acfe1d4e5d5595e0778e1581ed827a90f
Crul <rauljavier.vila@gmail.com>
2019-02-03 10:51:20 +0100

    naming refactor: data removed

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -6,85 +6,86 @@ agentsSpeed     = 1.5  -- TODO make speed work properly (now it applies to x,y n
 
 type Time = Int
 
-data WorldData = WorldData { time :: Int, agents :: [AgentData] } deriving (Show)
+data World = World { time :: Int, agents :: [Agent] } deriving (Show)
 data AgentType = Prey | Hunter | Food deriving (Eq, Show)
-data AgentData = AgentData {
+data Agent = Agent {
   agentType :: AgentType,
   agentId   :: Int,
   xAgPos    :: Float,
   yAgPos    :: Float
 }
 
-isFood :: AgentData -> Bool
+isFood :: Agent -> Bool
 isFood a = agentType a == Food
 
-isPrey :: AgentData -> Bool
+isPrey :: Agent -> Bool
 isPrey a = agentType a == Prey
 
-instance Show AgentData where
+instance Show Agent where
   show a = (show $ agentType a)
            ++ "[" ++ (show $ agentId a) ++ "]"
            ++ "(" ++ (show $ xAgPos a) ++ "," ++ (show $ yAgPos a) ++ ")"
 
-data MoveData = MoveData { xMove :: Float, yMove :: Float }
+data Move = Move { xMove :: Float, yMove :: Float }
 
-runTimeStep :: WorldData -> WorldData
-runTimeStep w = w { time = t, agents = ags }
+updateWorld :: World -> World
+updateWorld w = w { time = t, agents = ags }
   where
     t         = succ $ time w
-    agsAndMvs = map (runAgentStep w) (agents w)
-    ags       = map runMoveAction agsAndMvs
+    agsAndMvs = map (updateAgent w) (agents w)
+    ags       = map applyMoveAction agsAndMvs
 
-senseAgents :: AgentData -> WorldData -> [AgentData]
+senseAgents :: Agent -> World -> [Agent]
 senseAgents _ w = agents w  -- TODO filter agents based on agent position and vision-range parameter
 
-runAgentStep :: WorldData -> AgentData -> (AgentData, MoveData)
-runAgentStep w a@(AgentData Prey   _ _ _) = runTowardsType isFood w a
-runAgentStep w a@(AgentData Hunter _ _ _) = runTowardsType isPrey w a
-runAgentStep w a@(AgentData Food   _ _ _) = (a, MoveData 0 0)  -- TODO Food shouldn't do actions
+updateAgent :: World -> Agent -> (Agent, Move)
+updateAgent w a@(Agent Prey   _ _ _) = updateTowardsType isFood w a
+updateAgent w a@(Agent Hunter _ _ _) = updateTowardsType isPrey w a
+updateAgent w a@(Agent Food   _ _ _) = (a, Move 0 0)  -- TODO Food shouldn't do actions
 
-runTowardsType :: (AgentData -> Bool) -> WorldData -> AgentData -> (AgentData, MoveData)
-runTowardsType tf w a = ( nxtA, mv )
+updateTowardsType :: (Agent -> Bool) -> World -> Agent -> (Agent, Move)
+updateTowardsType tf w a = ( nxtA, mv )
   where
     ags  = senseAgents a w
     trgt = head $ filter tf ags
     mvTw = moveTowardsAg a trgt
-    mv   = MoveData (fst mvTw) (snd mvTw)
+    mv   = Move (fst mvTw) (snd mvTw)
     nxtA = a
 
-moveTowardsAg :: AgentData -> AgentData -> (Float, Float)
+moveTowardsAg :: Agent -> Agent -> (Float, Float)
 moveTowardsAg ag trgt = ( xMv, yMv )
   where
     moveAgTowardsAg = moveTowardsAgComp ag trgt
     xMv = moveAgTowardsAg xAgPos
     yMv = moveAgTowardsAg yAgPos
 
-moveTowardsAgComp :: AgentData -> AgentData -> (AgentData -> Float) -> Float
+moveTowardsAgComp :: Agent -> Agent -> (Agent -> Float) -> Float
 moveTowardsAgComp ag trgt agPFn =
   if (abs dst) < agentsSpeed
     then dst
     else agentsSpeed * (dst / abs dst)
   where dst = (agPFn trgt) - (agPFn ag)
 
-runMoveAction :: (AgentData, MoveData) -> AgentData
-runMoveAction (a, m) = a {
+applyMoveAction :: (Agent, Move) -> Agent
+applyMoveAction (a, m) = a {
     xAgPos = (xAgPos a) + (xMove m),
     yAgPos = (yAgPos a) + (yMove m)
   }
 
-initialWorld :: WorldData
-initialWorld = WorldData beginningOfTime
-  [
-    (AgentData Food   1 8 8),
-    (AgentData Prey   1 0 0),
-    (AgentData Hunter 1 4 0)
+initialAgents = [
+    (Agent Food   1 8 8),
+    (Agent Prey   1 0 0),
+    (Agent Hunter 1 4 0)
   ]
 
-timeline :: [WorldData]
-timeline = iterate runTimeStep initialWorld
+initialWorld :: World
+initialWorld = World beginningOfTime initialAgents
 
-runSimulationUpTo :: Time -> [WorldData]
-runSimulationUpTo i = takeWhile (\x -> time x <= i) timeline
+timeline :: [World]
+timeline = iterate updateWorld initialWorld
+
+updateTimeLineUpTo :: Time -> [World]
+updateTimeLineUpTo i = takeWhile (\x -> time x <= i) timeline
 
 runSimulation :: IO ()
-runSimulation = mapM_ print (runSimulationUpTo 10)
+runSimulation = mapM_ print (updateTimeLineUpTo 10)
